2cffd7faa5cfee773a68424fa4ca25b14bec8411
Icchi16 <duynghiemhn16@gmail.com>
2025-02-10 22:27:31 +0700

    fix calendar month select-able

--- app/(user)/calendar/components/MonthSeletor.tsx
+++ app/(user)/calendar/components/MonthSeletor.tsx
@@ -4,9 +4,8 @@ import dayjs, { Dayjs } from "dayjs";
 import { SwiperSlide, Swiper } from "swiper/react";
 import "dayjs/locale/vi";
 import Button from "@/components/global/Button";
-import { useRef } from "react";
-import { NavigationOptions } from "swiper/types";
-import { Navigation } from "swiper/modules";
+import { useRef, useState } from "react";
+import { Swiper as SwiperCore } from "swiper/types";
 import clsx from "clsx";
 import _ from "lodash";
 import { Icon } from "@iconify-icon/react/dist/iconify.mjs";
@@ -85,63 +84,117 @@ const MonthSelector = () => {
 
   const prevRef = useRef<HTMLButtonElement>(null);
   const nextRef = useRef<HTMLButtonElement>(null);
+  const swiperRef = useRef<SwiperCore | null>(null);
+  const [atEnd, setAtEnd] = useState(false);
+  const [activeMonth, setActiveMonth] = useState<number>(dayjs().month());
+
+  const handlePrevClick = () => {
+    if (!swiperRef.current) return;
+    if (activeMonth < 10) {
+      swiperRef.current.slidePrev();
+    }
+    setTimeout(() => {
+      setActiveMonth((prevMonth) => Math.max(0, prevMonth - 1));
+    }, 0);
+  };
+
+  const handleNextClick = () => {
+    if (!swiperRef.current) return;
+    swiperRef.current.slideNext();
+    setTimeout(() => {
+      setActiveMonth((prevMonth) =>
+        Math.min(prevMonth + 1, MonthArr.length - 1)
+      );
+    }, 0);
+  };
 
   return (
     <div className="font-[family-name:var(--body)]">
-      <div className="flex w-full justify-between items-center space-x-8">
+      <div className="flex w-full justify-between items-center space-x-2">
         <Button
           type="box"
           ref={prevRef}
           className="swiper-button-prev px-5 py-1"
+          onClick={handlePrevClick}
         >
           <Icon icon="pajamas:arrow-left" className="text-5xl" />
         </Button>
-        <Swiper
-          slidesPerView={"auto"}
-          modules={[Navigation]}
-          navigation={{
-            nextEl: nextRef.current!,
-            prevEl: prevRef.current!,
-          }}
-          onSwiper={(swiper) => {
-            // Delay execution for the refs to be defined
-            setTimeout(() => {
-              if (swiper.params && prevRef.current && nextRef.current) {
-                const navigation = swiper.params
-                  .navigation as NavigationOptions;
 
-                // Override prevEl & nextEl now that refs are defined
-                navigation.prevEl = prevRef.current;
-                navigation.nextEl = nextRef.current;
-
-                // Re-init navigation
-                swiper.navigation.destroy();
-                swiper.navigation.init();
-                swiper.navigation.update();
-              }
-            }, 0); // Ensure immediate execution
-          }}
+        {/* Swiper fade edge */}
+        <div
+          className={clsx(
+            "relative overflow-hidden transition-all duration-300",
+            !atEnd ? "edge-fade-right" : "edge-fade-left"
+          )}
         >
-          {MonthArr.map((month, index) => (
-            <SwiperSlide
-              key={month.id}
-              className={clsx(
-                "w-fit px-12 h-12 border-primary",
-                index !== _.last(MonthArr)?.id && "border-r-[3px]"
-              )}
-            >
-              <div className="flex h-full justify-between items-center">
-                <div className="uppercase font-light text-2xl flex justify-center w-full tracking-[0.2em]">
-                  {month.text}
+          <Swiper
+            slidesPerView={"auto"}
+            onSwiper={(swiper) => {
+              swiperRef.current = swiper;
+              //Shift to active month when initialize
+              swiper.slideTo(activeMonth, 0);
+
+              // Remove fade at end
+              swiper.on("progress", function () {
+                if (swiper.isEnd) {
+                  setAtEnd(true);
+                } else {
+                  setAtEnd(false);
+                }
+              });
+
+              // Handle Clock and shift to active slide
+              swiper.on("click", function () {
+                const clickedIndex = swiper.clickedIndex;
+                if (clickedIndex !== undefined && clickedIndex !== null) {
+                  swiper.slideTo(clickedIndex);
+                  setActiveMonth(dayjs(MonthArr[clickedIndex].date).month());
+                }
+              });
+            }}
+          >
+            {MonthArr.map((month, index) => (
+              <SwiperSlide
+                key={month.id}
+                className={clsx(
+                  "w-fit px-6 h-12 border-primary cursor-pointer",
+                  index !== _.last(MonthArr)?.id && "border-r-[3px]"
+                )}
+              >
+                <div className="flex h-full justify-between items-center relative">
+                  <div
+                    className={clsx(
+                      "transition-all duration-300 h-[3px] bg-primary absolute w-full bottom-0",
+                      activeMonth === dayjs(month.date).month()
+                        ? "opacity-100"
+                        : "opacity-0"
+                    )}
+                  ></div>
+                  {/* Invisible duplicate text with font-bold */}
+                  <div
+                    className={clsx(
+                      "absolute uppercase text-2xl flex justify-center w-full tracking-[0.2em] transition-all duration-300 hover:font-semibold",
+                      activeMonth === dayjs(month.date).month()
+                        ? "font-semibold"
+                        : "font-light"
+                    )}
+                  >
+                    {month.text}
+                  </div>
+                  {/* Visible text */}
+                  <div className="uppercase font-bold invisible text-2xl flex justify-center w-full tracking-[0.2em]">
+                    {month.text}
+                  </div>
                 </div>
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
         <Button
           type="box"
-          ref={prevRef}
+          ref={nextRef}
           className="swiper-button-prev px-5 py-1"
+          onClick={handleNextClick}
         >
           <Icon icon="pajamas:arrow-right" className="text-5xl" />
         </Button>
